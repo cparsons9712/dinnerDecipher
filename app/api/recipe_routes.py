c12a19f7167e5c75181adc1bf47eb538cea8d5c7
christine parsons <clong9712@gmail.com>
2023-11-29 20:54:02 -0500

    create post rt for recipe feature

--- app/api/recipe_routes.py
+++ app/api/recipe_routes.py
@@ -1,6 +1,9 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, session, request
 from flask_login import login_required
-from app.models import Recipe
+from flask_login import current_user, login_required
+from app.models import Recipe, db
+from app.forms import RecipeForm
+from .auth_routes import validation_errors_to_error_messages
 
 recipe_routes = Blueprint('recipes', __name__)
 
@@ -13,4 +16,19 @@ def all_recipes():
     recipes = Recipe.query.all()
     return {'recipes': [recipe.to_dict() for recipe in recipes]}
 
-
+@recipe_routes.route('/', methods=['POST'])
+@login_required
+def create_recipe():
+    """
+    Create a new database instance of a recipe.
+    """
+    form = RecipeForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        recipe = Recipe()
+        form.populate_obj(recipe)
+        recipe.user_id = current_user.id
+        db.session.add(recipe)
+        db.session.commit()
+        return recipe.to_dict()
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
