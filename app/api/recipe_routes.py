3a98b04bdf603f3cef38f587db6be7ce2c8f2c57
christine parsons <clong9712@gmail.com>
2023-12-01 20:28:44 -0500

    recipe delete API rt creation

--- app/api/recipe_routes.py
+++ app/api/recipe_routes.py
@@ -44,10 +44,24 @@ def update_recipe(recipe_id):
     recipe = Recipe.query.get_or_404(recipe_id)
 
     if form.validate_on_submit():
-        
+
         form.populate_obj(recipe)
         recipe.user_id = current_user.id
         db.session.add(recipe)
         db.session.commit()
         return recipe.to_dict()
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+
+@recipe_routes.route('<recipe_id>', methods=['PUT'])
+@login_required
+def delete_recipe(recipe_id):
+    """
+    Removes a recipe from the database
+    """
+    recipe = Recipe.query.filter(Recipe.id == recipe_id).first()
+    if recipe is None:
+        return {'errors': {'Not Found': 'A task with this id could not be located'}}, 404
+    db.session.delete(recipe)
+    db.session.commit()
+
+    return {'message': 'Recipe deleted!'}
