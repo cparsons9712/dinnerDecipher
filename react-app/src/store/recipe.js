207c623d3398629f11b427e0728443754d3d98a4
christine parsons <clong9712@gmail.com>
2023-12-13 20:00:04 -0500

    get all recipe thunk

--- react-app/src/store/recipe.js
+++ react-app/src/store/recipe.js
@@ -1,5 +1,33 @@
+const GET_RECIPES = "recipe/GET_RECIPES"
+
+const getAllRecipes = (recipes) => ({
+    type: GET_RECIPES,
+    payload: recipes,
+})
+
+export const getRecipes = () => async (dispatch) => {
+    const response = await fetch("/api/recipes/", {
+        headers: {
+            "Content-Type": "application/json",
+        }
+    });
+    if (response.ok){
+        const data = await response.json();
+        if(data.errors){
+            return;
+        }
+
+        dispatch(getAllRecipes(data))
+    }
+
+}
+
+
+
 export default function reducer(state = {}, action) {
 	switch (action.type) {
+        case GET_RECIPES:
+            return {recipes: action.payload}
 
 		default:
 			return state;
