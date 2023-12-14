b9b80e7b4b1a28fc1d036e0c937eca2299b00579
christine parsons <clong9712@gmail.com>
2023-12-13 20:14:45 -0500

    pull data into component from store

--- react-app/src/components/Recipes/index.js
+++ react-app/src/components/Recipes/index.js
@@ -1,6 +1,24 @@
+import { getRecipes } from "../../store/recipe"
+import React, { useState , useEffect} from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+
+
 function RecipeList() {
+    const dispatch = useDispatch();
+
+    useEffect (()=>{
+        // trigger the data retrieval from the backend to go into the store
+        dispatch(getRecipes())
+    })
+
+    // save the data in the store to a variable we can use.
+    const recipes = useSelector((state) => state.recipes)
+
+
     return (
         <h1>Recipes</h1>
-        
+
+
     )
 }
