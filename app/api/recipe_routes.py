518a0141a335c529b3f13736ead7f62e82dbdbfa
christine parsons <clong9712@gmail.com>
2024-02-01 21:07:23 -0500

    recipe put request

--- app/api/recipe_routes.py
+++ app/api/recipe_routes.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, jsonify, session, request
 from flask_login import login_required
 from flask_login import current_user, login_required
-from app.models import Recipe, db
-from app.forms import RecipeForm
+from app.models import Recipe, Direction, RecipeIngredient, db
+from app.forms import RecipeForm, RecipeIngredientForm, DirectionForm
 from .auth_routes import validation_errors_to_error_messages
 
 recipe_routes = Blueprint('recipes', __name__)
@@ -22,16 +22,54 @@ def create_recipe():
     """
     Create a new database instance of a recipe.
     """
-    form = RecipeForm()
-    form['csrf_token'].data = request.cookies['csrf_token']
-    if form.validate_on_submit():
+    # recieve the payload from the request
+    data = request.json
+    #
+    recipe_form = RecipeForm(data=data)
+    recipe_form['csrf_token'].data = request.cookies['csrf_token']
+
+    if recipe_form.validate_on_submit():
+        # Creating the new recipe instance with data from request
         recipe = Recipe()
-        form.populate_obj(recipe)
+        recipe_form.populate_obj(recipe)
         recipe.user_id = current_user.id
         db.session.add(recipe)
+
+        #Creating new direction instances for each direction for recipe in payload
+        directions_data = data.get('directions')
+        if directions_data:
+            # loop through every step in the set of directions
+            for step_data in directions_data:
+                # send threw flaskForm
+                direction_form = DirectionForm(data=directions_data)
+                if direction_form.validate():
+                    # in the case of no validation errors, create a new DB instance
+                    step = Direction(**step_data, recipe=recipe)
+                    db.session.add(step)
+                else:
+                    # in case a step of the directions throws a validation error
+                    return jsonify({'errors': direction_form.errors}), 400
+
+        # Create new recipeIngredient instance for each ingredient in payload
+        ingredients_data = data.get('ingredients')
+        if ingredients_data:
+            # loop through all the ingredients in the ingredients chunk sent in payload
+            for ingredient_data in ingredients_data:
+                # send each individual ingredient into the flask form for validation
+                ingredient_form = RecipeIngredientForm(data=ingredient_data)
+                if ingredient_form.validate():
+                    # Make a new database instance if the data set passes validations
+                    ingredient = RecipeIngredient(**ingredient_data, recipe=recipe)
+                    db.session.add(ingredient)
+                else:
+                    # return the errors to frontend if it doesnt validate
+                    return jsonify({'errors': ingredient_form.errors}), 400
+
+        # save the changes made to the database and return the new recipe in json formatt
         db.session.commit()
         return recipe.to_dict()
-    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+    # if there are errors with the recipe flask form validations return those to the frontend
+    return {'errors': validation_errors_to_error_messages(recipe_form.errors)}, 401
 
 @recipe_routes.route('<recipe_id>', methods=['PUT'])
 @login_required
--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,3 +1,5 @@
 from .login_form import LoginForm
 from .signup_form import SignUpForm
 from .recipe_form import RecipeForm
+from .recipeIngredient_form import RecipeIngredientForm
+from .direction_form import DirectionForm
